7bf6de034327d561dd266bdb590e774bd69a6a65
Mustansir Udaipurwala <mhusain@mhusa.in>
2021-02-16 23:56:18 +0400

    create_folds - DONE, train.py - WIP

---
+++ src/train.py
@@ -0,0 +1,28 @@
+from numpy.lib.index_tricks import AxisConcatenator
+import pandas as pd
+from sklearn import preprocessing
+
+TRAINING_DATA = None
+FOLD = None
+
+FOLD_MAPPING = {
+    0: [1, 2, 3, 4],
+    1: [0, 2, 3, 4],
+    2: [0, 1, 3, 4],
+    3: [0, 1, 2, 4],
+    4: [0, 1, 2, 3],
+}
+
+if __name__ == "__main__":
+    df = pd.read_csv(TRAINING_DATA)
+    train_df = df[df.kfold.isin(FOLD_MAPPING.get(FOLD))]
+    valid_df = df[df.kfold==FOLD]
+
+    ytrain = train_df.target.values
+    yvalid = valid_df.target.values
+
+    train_df = train_df.drop(["id", "target", "kfold"], axis=1)
+    valid_df = valid_df.drop(["id", "target", "kfold"], axis=1)
+
+    valid_df = valid_df[train_df.columns]
+
